3e5e318de7a8d95cb8a9f7708ef7b4493a5f5c31
sivakumar2999 <sivakumar2999@gmail.com>
2018-04-23 08:46:13 +0530

    Update Application.java

--- customer-service/src/main/java/pl/piomin/microservices/customer/Application.java
+++ customer-service/src/main/java/pl/piomin/microservices/customer/Application.java
@@ -2,10 +2,12 @@ package pl.piomin.microservices.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableFeignClients
 public class Application {
